a3cf967e118748f6e617848c77a22efb5bfbf2f4
geniuxy <2238384910@qq.com>
2025-01-06 20:53:02 +0800

    [25.1.6] FString::Printf test

--- Source/BlackWKVsAstroBot/Private/Item/Item.cpp
+++ Source/BlackWKVsAstroBot/Private/Item/Item.cpp
@@ -27,5 +27,13 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (GEngine)
+	{
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
+		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
+	}
+
 }
 
