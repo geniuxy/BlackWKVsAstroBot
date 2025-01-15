9754e6003138d6e1672d18b34d4bd170b19ebc24
geniuxy <2238384910@qq.com>
2025-01-16 00:39:06 +0800

    [25.1.15](88) animation instance c++

---
+++ Source/BlackWKVsAstroBot/Private/Character/KratosAnimInstance.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/KratosAnimInstance.h"
+
+#include "Character/Kratos.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UKratosAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	KratosCharacter = Cast<AKratos>(TryGetPawnOwner());
+	if (KratosCharacter)
+		KratosMovementComponent = KratosCharacter->GetCharacterMovement();
+}
+
+void UKratosAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (KratosMovementComponent)
+		GroundSpeed = UKismetMathLibrary::VSizeXY(KratosMovementComponent->Velocity);
+}
---
+++ Source/BlackWKVsAstroBot/Public/Character/KratosAnimInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "KratosAnimInstance.generated.h"
+
+class AKratos;
+class UCharacterMovementComponent;
+/**
+ * 
+ */
+UCLASS()
+class BLACKWKVSASTROBOT_API UKratosAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	AKratos* KratosCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category=Movement)
+	UCharacterMovementComponent* KratosMovementComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category=Movement)
+	float GroundSpeed;
+};
