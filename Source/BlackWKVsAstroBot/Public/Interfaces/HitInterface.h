75815c432f5697ad4082586abc3ca8432bbd7db5
geniuxy <2238384910@qq.com>
2025-01-26 00:04:46 +0800

    [25.1.25](132) Unreal Interfaces

---
+++ Source/BlackWKVsAstroBot/Public/Interfaces/HitInterface.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "HitInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UHitInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class BLACKWKVSASTROBOT_API IHitInterface
+{
+	GENERATED_BODY()
+
+public:
+	// if pure virtual function of interface, need + " = 0"
+	virtual void GetHit() = 0;
+};
