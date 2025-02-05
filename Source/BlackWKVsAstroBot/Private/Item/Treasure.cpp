de84cf6326e99b2569c2a63e04213ae213cdb23c
geniuxy <2238384910@qq.com>
2025-02-05 20:18:09 +0800

    [25.2.5](154) Treasure

---
+++ Source/BlackWKVsAstroBot/Private/Item/Treasure.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/Treasure.h"
+
+#include "Character/Hero.h"
+#include "Kismet/GameplayStatics.h"
+
+void ATreasure::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool bFromSweep,
+                                const FHitResult& SweepResult)
+{
+	AHero* Hero = Cast<AHero>(OtherActor);
+	if (Hero)
+	{
+		if (PickupSound)
+			UGameplayStatics::PlaySoundAtLocation(this, PickupSound, GetActorLocation());
+
+		Destroy();
+	}
+}
---
+++ Source/BlackWKVsAstroBot/Public/Item/Treasure.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/Item.h"
+#include "Treasure.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKWKVSASTROBOT_API ATreasure : public AItem
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                             UPrimitiveComponent* OtherComp, int OtherBodyIndex, bool bFromSweep,
+	                             const FHitResult& SweepResult) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = Sound)
+	USoundBase* PickupSound;
+};
